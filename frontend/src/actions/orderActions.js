18cbb4212602be0ed1fa1f7c1f7c3018b4decddc
neangsamoun01 <80339215+neangsamoun01@users.noreply.github.com>
2021-04-07 08:02:32 +0700

    Update orderActions.js

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -109,13 +109,13 @@ export const listOrderMine = () => async (dispatch, getState) => {
     dispatch({ type: ORDER_MINE_LIST_FAIL, payload: message });
   }
 };
-export const listOrders = () => async (dispatch, getState) => {
+export const listOrders = ({ seller = '' }) => async (dispatch, getState) => {
   dispatch({ type: ORDER_LIST_REQUEST });
   const {
     userSignin: { userInfo },
   } = getState();
   try {
-    const { data } = await Axios.get('/api/orders', {
+    const { data } = await Axios.get(`/api/orders?seller=${seller}`, {
       headers: { Authorization: `Bearer ${userInfo.token}` },
     });
     console.log(data);
@@ -128,24 +128,24 @@ export const listOrders = () => async (dispatch, getState) => {
     dispatch({ type: ORDER_LIST_FAIL, payload: message });
   }
 };
-export const deleteOrder = (orderId) => (dispatch, getState) => {
-  dispatch({ type: ORDER_DELETE_REQUEST, payload: orderId});
-  const {userSignin:{userInfo}} = getState();
-  try{
-    const {data} = Axios.delete(`/api/orders/${orderId}`, {
-      headers: {Authorization: `Bearer ${userInfo.token}`}
+export const deleteOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({ type: ORDER_DELETE_REQUEST, payload: orderId });
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  try {
+    const { data } = Axios.delete(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
     });
-    dispatch({type:ORDER_DELETE_SUCCESS, payload:data})
-  } catch(error) {
+    dispatch({ type: ORDER_DELETE_SUCCESS, payload: data });
+  } catch (error) {
     const message =
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message;
-    dispatch({ type: ORDER_DELETE_FAIL, payload:message})
+    dispatch({ type: ORDER_DELETE_FAIL, payload: message });
   }
 };
-
-
 
 export const deliverOrder = (orderId) => async (dispatch, getState) => {
   dispatch({ type: ORDER_DELIVER_REQUEST, payload: orderId });
@@ -169,4 +169,3 @@ export const deliverOrder = (orderId) => async (dispatch, getState) => {
     dispatch({ type: ORDER_DELIVER_FAIL, payload: message });
   }
 };
-
